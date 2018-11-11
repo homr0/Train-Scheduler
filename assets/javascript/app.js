07debc9d119097ccc2b90c02dc047e0b81600c01
Rebecca Hom <rebecca.hom001@gmail.com>
2018-11-10 23:11:28 -0800

    added in code for loading train data

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -13,42 +13,57 @@ $(document).ready(function() {
     // Assign the reference to the database
     var database = firebase.database();
 
-    // Format for data.
-    // trainName : {
-    //  arrival: arrivalTime,
-    //  destination: trainDest,
-    //  frequency: trainFreq
-    // }
-    
+    // Every minute, check if the train has arrived and then update the arrival time.
+    function trainArriving() {
+
+    }
+    trainArriving();
+
     // At the initial load and subsequent value changes, get a snapshot of the stored data.
-    database.ref().on("value", function(snapshot) {
-        // If Firebase has a listing for trains, then get all values
-        if(snapshot.child("trains").exists()) {
-            var trains = snapshot.child("trains").val();
-            console.log(trains);
+    database.ref().child("trains").on("child_added", function(trains) {
+        // For each train, print out the information
+        $(trains.val()).each(function(key, value) {
+            var trainRow = $("<tr>");
 
-            // For each train, print out the information
-            $(trains).each(function(key, value) {
-                let train = Object.keys(value)[0];
-                console.log(key, train, value);
+            var trainName = $("<th>").text(value.name);
 
-                console.log(value[train].arrival);
-                var trainRow = $("<tr>");
+            var trainDest = $("<td>").text(value.destination);
 
-                var trainName = $("<th>").text(train);
+            var trainFreq = $("<td>").text(value.frequency);
 
-                var trainDest = $("<td>").text(value[train].destination);
+            let timeArr = moment(value.arrival, "HH:mm");
+            var trainArr = $("<td>").text(timeArr.format("hh:mm A"));
 
-                var trainFreq = $("<td>").text(value[train].frequency);
+            // Calculate the train's arrival time
+            var trainNext = timeArr.diff(moment(), 'minutes');
 
-                var trainNext = $("<td>").text(value[train].arrival);
+            var trainAway = $("<td>").text(trainNext);
 
-                var trainAway = $("<td>").text("Coming soon!");
+            $(trainRow).append(trainName, trainDest, trainFreq, trainArr, trainAway);
 
-                $(trainRow).append(trainName, trainDest, trainFreq, trainNext, trainAway);
+            $("#trainTable tbody").append(trainRow);
+        });
+    });
 
-                $("#trainTable tbody").append(trainRow);
-            });
-        }
+    // When a user submits a new train, then add in the info to the database
+    $("#newTrain").on("click", function(e) {
+        e.preventDefault();
+
+        // Get the inputs
+        var trainName = $("#trainName").val().trim();
+        var trainDest= $("#trainDest").val().trim();
+        var trainArrival = $("#trainArr").val().trim();
+        var trainFreq = $("#trainFreq").val().trim();
+
+        // Puts the train into the database.
+        database.ref().child("trains").push({
+            name: trainName,
+            destination: trainDest,
+            arrival: trainArrival,
+            frequency: trainFreq
+        });
+
+        // Clears the inputs
+        $("#trainName, #trainDest, #trainArr, #trainFreq").val("");
     });
 });
