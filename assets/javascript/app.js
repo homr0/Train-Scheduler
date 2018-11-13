3b9c0963e11e2b697076d160c2e92709381e2e26
Rebecca Hom <rebecca.hom001@gmail.com>
2018-11-13 14:44:38 -0800

    added in Google sign-in code for Firebase

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -10,6 +10,28 @@ $(document).ready(function() {
     };
     firebase.initializeApp(config);
 
+    // User authentication for Google
+    var provider = new firebase.auth.GoogleAuthProvider();
+    firebase.auth().useDeviceLanguage();
+    firebase.auth().signInWithPopup(provider).then(function(result) {
+        // This gives you a Google Access Token. You can use it to access the Google API.
+        var token = result.credential.accessToken;
+        // The signed-in user info.
+        var user = result.user;
+        // ...
+        }).catch(function(error) {
+        // Handle Errors here.
+        var errorCode = error.code;
+        var errorMessage = error.message;
+        // The email of the user's account used.
+        var email = error.email;
+        // The firebase.auth.AuthCredential type that was used.
+        var credential = error.credential;
+        
+        console.log(email, credential);
+        console.log(errorCode + ": " + errorMessage);
+    });
+
     // Assign the reference to the database
     var database = firebase.database();
 
