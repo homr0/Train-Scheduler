07b245d48b69a368143ab12144d4cdd35c14e469
Rebecca Hom <rebecca.hom001@gmail.com>
2018-11-13 17:29:57 -0800

    temporarily removed sign in

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -16,28 +16,28 @@ $(document).ready(function() {
     //firebase.auth().signInWithRedirect(provider);
 
     //firebase.auth().getRedirectResult().then(function(result) {
-    firebase.auth().signInWithPopup(provider).then(function(result) {
-        if (result.credential) {
-          // This gives you a Google Access Token. You can use it to access the Google API.
-          var token = result.credential.accessToken;
-          // ...
-        }
-        // The signed-in user info.
-        var user = result.user;
+    // firebase.auth().signInWithPopup(provider).then(function(result) {
+    //     if (result.credential) {
+    //       // This gives you a Google Access Token. You can use it to access the Google API.
+    //       var token = result.credential.accessToken;
+    //       // ...
+    //     }
+    //     // The signed-in user info.
+    //     var user = result.user;
 
-        console.log(user + ": " + token);
-      }).catch(function(error) {
-        // Handle Errors here.
-        var errorCode = error.code;
-        var errorMessage = error.message;
-        // The email of the user's account used.
-        var email = error.email;
-        // The firebase.auth.AuthCredential type that was used.
-        var credential = error.credential;
+    //     console.log(user + ": " + token);
+    //   }).catch(function(error) {
+    //     // Handle Errors here.
+    //     var errorCode = error.code;
+    //     var errorMessage = error.message;
+    //     // The email of the user's account used.
+    //     var email = error.email;
+    //     // The firebase.auth.AuthCredential type that was used.
+    //     var credential = error.credential;
         
-        console.log(email + ": " + credential);
-        console.log(errorCode + ": " + errorMessage);
-      });
+    //     console.log(email + ": " + credential);
+    //     console.log(errorCode + ": " + errorMessage);
+    //   });
 
     // Assign the reference to the database
     var database = firebase.database();
