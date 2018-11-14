062c757b3caa17a262262eaf7117ab314fa3205d
Rebecca Hom <rebecca.hom001@gmail.com>
2018-11-13 17:18:52 -0800

    changed Google login to be a redirect page, not a pop-up

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -13,13 +13,19 @@ $(document).ready(function() {
     // User authentication for Google
     var provider = new firebase.auth.GoogleAuthProvider();
     firebase.auth().useDeviceLanguage();
-    firebase.auth().signInWithPopup(provider).then(function(result) {
-        // This gives you a Google Access Token. You can use it to access the Google API.
-        var token = result.credential.accessToken;
+    firebase.auth().signInWithRedirect(provider);
+
+    firebase.auth().getRedirectResult().then(function(result) {
+        if (result.credential) {
+          // This gives you a Google Access Token. You can use it to access the Google API.
+          var token = result.credential.accessToken;
+          // ...
+        }
         // The signed-in user info.
         var user = result.user;
-        // ...
-        }).catch(function(error) {
+
+        console.log(user + ": " + token);
+      }).catch(function(error) {
         // Handle Errors here.
         var errorCode = error.code;
         var errorMessage = error.message;
@@ -28,9 +34,9 @@ $(document).ready(function() {
         // The firebase.auth.AuthCredential type that was used.
         var credential = error.credential;
         
-        console.log(email, credential);
+        console.log(email + ": " + credential);
         console.log(errorCode + ": " + errorMessage);
-    });
+      });
 
     // Assign the reference to the database
     var database = firebase.database();
